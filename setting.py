a748e12e47117bc198f6a58e2c3051e6ac5844a1
ParselShip <62086334+ParselShip@users.noreply.github.com>
2020-03-12 17:08:40 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,34 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-DIR = ""
+DIR = "存放图片路径"
 DOMAIN = [
-    'www.wyssangyu.com',
-    'www.083869.com',
-    'www.zhendazc.com',
-    'www.czwwyh.com',
-    'www.xmyg100js.com',
-    'www.qdlsnjl.com',
-    'www.ptxdmy.com',
-    'www.hbscgd888.com',
-    'www.huizhongwenhua.com',
-    'www.hbrkljyzx.com',
-    'www.huamingis.com',
-    'www.changshalongchi.com',
-    'www.dszm-lighting.com',
-    'www.ythuishan.com',
-    'www.sxzhentai.com',
-    'www.wzetdzbike.com',
-    'www.gzzhengjing.com',
-    'www.wyqcmy.com',
-    'www.yamseiki.com',
-    'www.airuicar.com',
-    'www.tsnlzd.com',
-    'www.szzdcx.com',
-    'www.fufang120.com',
-    'www.feimakd.com',
-    'www.nxxsqt.com',
-    'www.nanshanjige.com',
-    'www.168hy.net'
+# 此处存放目标站点
 ]
 # 数量 数据类型：int 写错会报错
 COUNT = 1
